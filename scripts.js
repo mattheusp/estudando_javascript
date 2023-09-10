5e76a213ecb4548d783cc21be2abedcc1575f295
Mattheus <mattheusp382@gmail.com>
2023-09-09 22:45:25 -0300

    git add

--- scripts.js
+++ scripts.js
@@ -5,3 +5,50 @@ let fruta = 'laranja';
 let cor = 'azul';
 let marca = 'apple';
 let comida = 'arroz com ovo';
+
+
+
+// TIPOS DE DADOS EM VARIÁVEIS
+
+// string
+let nome = "Mattheus";
+
+
+// integers
+// * numeros
+let idade = 20;
+
+// floats 
+// * pontos flutuantes
+let contaBancaria = 987.00;
+
+// boleano
+
+// true
+// false
+
+let MattheusCabeloPreto = false;
+
+let MattheusUsaCueca = true;
+
+// Arrays 
+// Contém tipos de variaveis
+
+let cores = ['azul', 'roxo', 'vermelho', 'verde'];
+let frutas = ['Maça', 'Banana', 'Melancia'];
+
+// Acessar um item espeicifico dentro de um array
+frutas[0]
+
+// objetos
+let xicara = {cor: "Branca", material: "porcelana", peso: 100}
+
+// Acessando uma propriedade de um objeto
+xicara.peso
+
+
+
+// Resolução do desafio
+
+let fiatUno = {ano: "2014", modelo: "fiatuno", linha: "nao", cores: ["Branco", "vermelho", "prata"]};
+
